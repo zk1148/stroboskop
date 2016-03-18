7ad7428071870279e84112c549aa59f4bf797d47
Ziga <zk1148@student.uni-lj.si>
2016-03-18 18:47:42 +0000

    Dinamika gumba za ustavitev stroboskopa

--- skripta.js
+++ skripta.js
@@ -40,6 +40,11 @@ window.addEventListener('load', function() {
 	
 	var stop = function(event) {
 		ustavi = true;
+		
+		var start = document.querySelector("#start");
+ 		start.innerHTML = "Zaženi stroboskop";
+ 		start.removeEventListener('click',stop);
+ 		start.addEventListener('click', zagon);
 	}
 	
 	var zagon = function(event) {
